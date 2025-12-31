f17ebe7ec817c2faf4fc4bf54a40cc0987b07801
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-12-31 15:45:32 +0800

    fix(website): Correct the base path

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import historyDataPlugin from './plugins/history_data'
 
 export default defineConfig({
+  base: './', // Use a relative base path
   build: {
     chunkSizeWarningLimit: 1000,
   },
