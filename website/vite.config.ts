7fc3ecc9e43764b85b25c2aeb1b697cac3cc274b
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-12-31 09:24:35 +0800

    chore: Increase build.chunkSizeWarningLimit for vite

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,5 +5,8 @@ import { defineConfig } from 'vite'
 import historyDataPlugin from './plugins/history_data'
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   plugins: [react(), tailwindcss(), historyDataPlugin()],
 })
