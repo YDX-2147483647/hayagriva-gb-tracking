a9607dd4beab6c0a742d54f754a602416e09c5dd
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2026-01-11 22:06:11 +0800

    docs: add issue/gap links

--- website/src/category_description.tsx
+++ website/src/category_description.tsx
@@ -1,16 +1,17 @@
 import type { Category } from './types'
+import ExternalLink from './ExternalLink'
 
 export const descriptions: Record<Category, JSX.Element> = {
   lang: (
-    <span>
+    <ExternalLink href="https://gap.zhtyp.art/#et-al-lang">
       <code>等</code>/<code>et al.</code>等语言翻译问题
-    </span>
+    </ExternalLink>
   ),
   case: <span>大小写问题（例：英文机构名称被全大写）</span>,
   卷: (
-    <span>
+    <ExternalLink href="https://gap.zhtyp.art/#csl-is-numeric">
       多<code>卷</code>字（例：<code>第4册</code>误为<code>卷 第4册</code>）
-    </span>
+    </ExternalLink>
   ),
   han_space: (
     <span>
@@ -18,9 +19,9 @@ export const descriptions: Record<Category, JSX.Element> = {
     </span>
   ),
   code_space: (
-    <span>
+    <ExternalLink href="https://gap.zhtyp.art/#bib-num-trim-before">
       代码中的空格错误（例：<code>GB/T 123</code>误为<code>GB/T123</code>）
-    </span>
+    </ExternalLink>
   ),
   escape: (
     <span>
@@ -28,15 +29,15 @@ export const descriptions: Record<Category, JSX.Element> = {
     </span>
   ),
   num: (
-    <span>
+    <ExternalLink href="https://gap.zhtyp.art/#bib-num-trim-before">
       数码格式错误（例：<code>011</code>误为<code>11</code>）
-    </span>
+    </ExternalLink>
   ),
   punct: (
-    <span>
+    <ExternalLink href="https://github.com/typst/hayagriva/issues/180">
       标点符号缺少或错误（例：<code>[J].</code>缺点或<code>北京: 中华书局</code>
       缺冒号）
-    </span>
+    </ExternalLink>
   ),
   // Unknown is always shown last, so it's okay to use 以上.
   Unknown: <span>以上未列出的差异</span>,
